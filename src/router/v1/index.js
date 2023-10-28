30b917f8064342e465d95a8e12ed6b778a779a62
berchiche willem <berwillempro@gmail.com>
2023-10-28 02:46:18 +0100

    subs

--- src/router/v1/index.js
+++ src/router/v1/index.js
@@ -5,5 +5,6 @@ router.use("/auth/user", require("../../apps/main/routes/AuthUser"));
 router.use("/auth/magasin", require("../../apps/main/routes/AuthMagasin"));
 router.use("/user", require("../../apps/main/routes/User"));
 router.use("/service", require("../../apps/main/routes/Service"));
+router.use("/subscription", require("../../apps/main/routes/Subscription"));
 
 module.exports = router;
