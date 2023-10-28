8f3c0e16a010c4ff203f7e606dd7d02d08b18924
Wassim <512.amosimo@gmail.com>
2023-10-28 10:49:18 +0100

    Category implemented

--- src/router/v1/index.js
+++ src/router/v1/index.js
@@ -6,4 +6,5 @@ router.use("/auth/magasin", require("../../apps/main/routes/AuthMagasin"));
 router.use("/user", require("../../apps/main/routes/User"));
 router.use("/service", require("../../apps/main/routes/Service"));
 router.use("/newsletter", require("../../apps/main/routes/NewsLetter"))
+router.use("/categories", require("../../apps/main/routes/Category"))
 module.exports = router;
