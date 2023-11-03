aa9bed856ed5a597b67be92298aa79c34e1388a9
Wassim <512.amosimo@gmail.com>
2023-11-03 01:25:08 +0100

    fixes

--- src/apps/main/controllers/MagasinController.js
+++ src/apps/main/controllers/MagasinController.js
@@ -2,7 +2,7 @@ const Magasin = require("../models/Magasin");
 
 exports.getAllMagasin = (req, res) => {
   Magasin.find({})
-    .limit(req.body.num ? req.body.num : 4)
+    .limit(req.query.num ? req.query.num : 4)
     .then((magasins) => {
       res.send(magasins.map((magasin) => ({ name: magasin.magasinName })));
     });
