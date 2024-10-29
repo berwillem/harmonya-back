19287335ac6749851e14867c1a3a30e26cfe82a2
Wassim <512.amosimo@gmail.com>
2024-10-29 17:30:04 +0100

    notification types

--- src/apps/main/controllers/NotificationController.js
+++ src/apps/main/controllers/NotificationController.js
@@ -1,6 +1,12 @@
 // const { notifyGlobal } = require("../../../helpers/notificationUtils");
 const io = require("../../../config/socket");
-const { notifyGlobal, notifyAllMagains, notifyAllUsers, notifyMagasin, notifyUser } = require("../../../helpers/notificationUtils");
+const {
+  notifyGlobal,
+  notifyAllMagains,
+  notifyAllUsers,
+  notifyMagasin,
+  notifyUser,
+} = require("../../../helpers/notificationUtils");
 const Notification = require("../models/Notification");
 
 exports.readNotification = async (req, res) => {
@@ -12,102 +18,108 @@ exports.readNotification = async (req, res) => {
     notification.read = true;
     await notification.save();
     return res.status(200).json(notification);
-  } catch (err) { 
+  } catch (err) {
     return res.status(500).json({ message: "Failed to read notification" });
   }
-}
+};
 
 exports.getNotificationsByUser = async (req, res) => {
-  const { userId } = req.params
+  const { userId } = req.params;
+  const { type } = req.query;
+  let filter = {
+    $or: [
+      { targetType: "global" },
+      { targetType: "group", targetGroup: "users" },
+      { targetType: "user", targetUsers: userId },
+    ],
+  };
+  if (type) {
+    filter = {
+      ...filter,
+      type,
+    };
+  }
   try {
-    const notifications = await Notification.find({
-      $or: [
-        { targetType: 'global' },
-        { targetType: 'group', targetGroup: "users" },
-        { targetType: 'user', targetUsers: userId }
-      ]
-    }).sort({ createdAt: -1 });
+    const notifications = await Notification.find(filter).sort({
+      createdAt: -1,
+    });
     return res.status(200).json(notifications);
   } catch (err) {
     return res.status(500).json({ message: "Failed to get notifications" });
   }
-}
+};
 
 exports.getNotificationsByMagasin = async (req, res) => {
-  const { magasinId } = req.params
+  const { magasinId } = req.params;
   try {
     const notifications = await Notification.find({
       $or: [
-        { targetType: 'global' },
-        { targetType: 'group', targetGroup: "magasins" },
-        { targetType: 'magasin', targetMagasin: magasinId }
-      ]
+        { targetType: "global" },
+        { targetType: "group", targetGroup: "magasins" },
+        { targetType: "magasin", targetMagasin: magasinId },
+      ],
     }).sort({ createdAt: -1 });
 
     return res.status(200).json(notifications);
   } catch (err) {
     return res.status(500).json({ message: "Failed to get notifications" });
   }
-}
+};
 
 exports.globalNotification = async (req, res) => {
-  const {title, message, payload = {}, type = "info"} = req.body
+  const { title, message, payload = {}, type = "info" } = req.body;
   try {
-    await notifyGlobal({title, message, payload, type})
-    return res.status(200).json({message: "Notification sent"})
-  }catch(error){
-    console.log(error)
-    return res.status(500).json({message: "Failed to send notification"})
+    await notifyGlobal({ title, message, payload, type });
+    return res.status(200).json({ message: "Notification sent" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Failed to send notification" });
   }
-}
+};
 
 exports.allMagasinsNotification = async (req, res) => {
-  const {title, message, payload = {}, type = "info"} = req.body
+  const { title, message, payload = {}, type = "info" } = req.body;
   try {
-    await notifyAllMagains({title, message, payload, type})
-    
-    return res.status(200).json({message: "Notification sent"})
+    await notifyAllMagains({ title, message, payload, type });
 
-  }catch(error){
-    console.log(error)
-    return res.status(500).json({message: "Failed to send notification"})
+    return res.status(200).json({ message: "Notification sent" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Failed to send notification" });
   }
-}
+};
 
 exports.allUsersNotification = async (req, res) => {
-  const {title, message, payload = {}, type = "info"} = req.body
+  const { title, message, payload = {}, type = "info" } = req.body;
   try {
-    await notifyAllUsers({title, message, payload, type})
-    return res.status(200).json({message: "Notification sent"})
-
-  }catch(error){
-    console.log(error)
-    return res.status(500).json({message: "Failed to send notification"})
+    await notifyAllUsers({ title, message, payload, type });
+    return res.status(200).json({ message: "Notification sent" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Failed to send notification" });
   }
-}
+};
 
 exports.userNotification = async (req, res) => {
-  const {title, message, payload = {}, type = "info"} = req.body
-  const {userId} = req.params
+  const { title, message, payload = {}, type = "info" } = req.body;
+  const { userId } = req.params;
   try {
-    await notifyUser({title, message, payload, type, userId})
-    return res.status(200).json({message: "Notification sent"})
-
-  }catch(error){
-    console.log(error)
-    return res.status(500).json({message: "Failed to send notification"})
+    await notifyUser({ title, message, payload, type, userId });
+    return res.status(200).json({ message: "Notification sent" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Failed to send notification" });
   }
-}
+};
 
 exports.magasinNotification = async (req, res) => {
-  const {title, message, payload = {}, type = "info"} = req.body
-  const {magasinId} = req.params
+  const { title, message, payload = {}, type = "info" } = req.body;
+  const { magasinId } = req.params;
   try {
-    await notifyMagasin({title, message, payload, type, magasinId})
-    return res.status(200).json({message: "Notification sent"})
-
-  }catch(error){
-    console.log(error)
-    return res.status(500).json({message: "Failed to send notification"})
+    await notifyMagasin({ title, message, payload, type, magasinId });
+    return res.status(200).json({ message: "Notification sent" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Failed to send notification" });
   }
-}
+};
--- src/apps/main/models/Notification.js
+++ src/apps/main/models/Notification.js
@@ -1,61 +1,64 @@
-const mongoose =  require("mongoose");
+const mongoose = require("mongoose");
 
-const NotificationSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
+const NotificationSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    targetType: {
+      type: String,
+      enum: ["global", "group", "user", "magasin"],
+      required: true,
+    },
+    targetGroup: {
+      type: String,
+      enum: ["users", "magasins"],
+      required: function () {
+        return this.targetType === "group";
+      },
+    },
+    targetUser: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: function () {
+        return this.targetType === "user";
+      },
+    },
+    targetMagasin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Magasin",
+      required: function () {
+        return this.targetType === "magasin";
+      },
+    },
+    payload: {
+      type: Object,
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: ["booking", "promotion", "info"],
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+    createdAt: {
+      type: Date,
+      expires: 24 * 60 * 60 * 30,
+      default: Date.now,
+    },
   },
-  message: {
-    type: String,
-    required: true
-  },
-  targetType: {
-    type: String,
-    enum: ['global', 'group', 'user', 'magasin'],
-    required: true
-  },
-  targetGroup: {
-    type: String,
-    enum: ['users', 'magasins'],
-    required: function() {
-      return this.targetType === 'group';
-    }
-  },
-  targetUser: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: function() {
-      return this.targetType === 'user';
-    }
-  },
-  targetMagasin: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Magasin',
-    required: function() {
-      return this.targetType === 'magasin';
-    }
-  },
-  payload: {
-    type: Object
-  },
-  type : {
-    type: String,
-    required: true,
-    enum: ["reschedule", "success", "failure", "info"]
-  },
-  read: {
-    type: Boolean,
-    default: false
-  },
-  createdAt: {
-    type: Date,
-    expires: 24*60*60*30,
-    default: Date.now,
-  },
-}, {
-  timestamps:true
-})
+  {
+    timestamps: true,
+  }
+);
 
 const Notification = mongoose.model("Notification", NotificationSchema);
 
-module.exports = Notification
+module.exports = Notification;
